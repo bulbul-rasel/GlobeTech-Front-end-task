d227587769c1f6c0a250d4a617e467c5b608c502
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-07-29 01:30:14 +0600

    add login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import navigator from '../asset/Vector 1.svg'
+import eye from '../asset/Eye.svg'
+import './Login.css'
 
 const Login = () => {
     return (
@@ -8,41 +10,42 @@ const Login = () => {
                 <h1 className='uppercase text-center text-blue-500 font-Montserrat font-bold text-2xl'>Glovetech</h1>
                 <h1 className='uppercase text-center text-blue-500 font-Montserrat font-bold text-2xl'>Companey Limited</h1>
                 <p className='text-white text-xs text-center py-14 font-Montserrat tracking-widest'>Make yourself digitalized & more effecient</p>
-                <button className='pt-96 px-28'>
+                <button className='pt-96 px-28 '>
 
                     <img src={navigator} alt="" />
                 </button>
             </div>
             <div class="col-span-3 ">
                 <div className='h-screen flex bg-gray-bg1'>
-                    <div className='w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-16'>
-                        <h1 className='text-2xl font-medium text-primary mt-4 mb-12 text-center'>
-                            Log in to your account 🔐
+                    <div className='w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-10 px-6'>
+                        <h1 className='text-3xl font-light text-black my-6 text-center font-Montserrat'>
+                            Welcome
                         </h1>
+                        <p className='text-xs font-light text-center font-Montserrat my-4 text-gray-400'>Sign in to your account</p>
 
                         <form >
                             <div>
-                                <label htmlFor='email'>Email</label>
                                 <input
                                     type='email'
-                                    className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                                    className={`w-full p-2 text-primary border rounded-lg outline-none text-sm transition duration-150 ease-in-out mb-4`}
                                     id='email'
-                                    placeholder='Your Email'
+                                    placeholder='Email'
                                 />
                             </div>
-                            <div>
-                                <label htmlFor='password'>Password</label>
+                            {/* <div class="relative">
                                 <input
                                     type='password'
                                     className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                                     id='password'
-                                    placeholder='Your Password'
+                                    placeholder='Password'
                                 />
-                            </div>
+                                <img className='fa-eye' src={eye} alt="" />
+                            </div> */}
+                            <div class="relative"> <input class="pass1 h-10 w-full border mt-3 text-sm rounded-lg outline-none p-2" type="password" placeholder="Password" /> <img className='fa-eye' src={eye} alt="" /> </div>
 
                             <div className='flex justify-center items-center mt-6'>
                                 <button
-                                    className={`bg-green py-2 px-4 text-sm text-white rounded border border-green focus:outline-none focus:border-green-dark`}
+                                    className="btn bg-sky-500 border-none font-bold rounded-md w-full px-8 text-white"
                                 >
                                     Login
                                 </button>
